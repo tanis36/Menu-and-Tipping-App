7defa06e1b7ec3c0eaa97969b3de2b6b21dde744
tanis <tk009@lvc.edu>
2024-11-14 16:24:00 -0500

    Basic UI set up

--- Client/qr-menu-tip/src/App.js
+++ Client/qr-menu-tip/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ChakraProvider, Box, Button, VStack, Heading } from '@chakra-ui/react';
+// import { useStripe, Elements } from '@stripe/react-stripe-js';
+// import { loadStripe } from '@stripe/stripe-js';
+
+//TODO: get and add a key
+// const stripePromise = loadStripe('');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box textAlign="center" py={10}>
+        <VStack spacing={5}>
+          <Heading size="lg" onClick={() => window.location.href = '/'}>Bar 62</Heading>
+          <Button colorScheme="teal" onClick={() => window.location.href = '/menu'}>Menu</Button>
+          <Button colorScheme="blue" onClick={() => window.location.href = '/tip'}>Tip</Button>
+        </VStack>
+      </Box>
+    </ChakraProvider>
   );
 }
 
